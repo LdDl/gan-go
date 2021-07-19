ec94880c72838f41672fa7c65deb324d163ea773
Dimitrii Lopanov <sexykdi@gmail.com>
2021-07-19 17:50:14 +0300

    add HuberLoss (it's pseudoish actually

--- loss.go
+++ loss.go
@@ -151,3 +151,55 @@ func L1Loss(a, b *gorgonia.Node, reduction ...LossReduction) (*gorgonia.Node, er
 		return nil, fmt.Errorf("Reduction type %d is not supported", reductionDefault)
 	}
 }
+
+// HuberLoss See ref. https://en.wikipedia.org/wiki/Huber_loss
+// This is actually Pseudo Huber Loss - see ref. https://en.wikipedia.org/wiki/Huber_loss#Pseudo-Huber_loss_function
+// Delta value type should match Dtype of provided nodes. tensor.Float32 -> float32, tensor.Float64 -> float64 and etc.
+// Default reduction is 'mean'
+func HuberLoss(a, b *gorgonia.Node, delta interface{}, reduction ...LossReduction) (*gorgonia.Node, error) {
+	deltaScalar := gorgonia.NewScalar(a.Graph(), a.Dtype(), gorgonia.WithValue(delta))
+	sqrDelta := gorgonia.NewScalar(a.Graph(), a.Dtype(), gorgonia.WithValue(delta))
+	oneScalar := gorgonia.NewScalar(a.Graph(), a.Dtype(), gorgonia.WithValue(1.0))
+
+	sub, err := gorgonia.Sub(a, b)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (A-B)")
+	}
+
+	div, err := gorgonia.Div(sub, deltaScalar)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (X/delta)")
+	}
+	sqr, err := gorgonia.Square(div)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (x^2)")
+	}
+	addOneScalar, err := gorgonia.Add(oneScalar, sqr)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (1.+X)")
+	}
+	sqrt, err := gorgonia.Sqrt(addOneScalar)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do √x")
+	}
+	subOneScalar, err := gorgonia.Sub(sqrt, oneScalar)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (X.-1)")
+	}
+	matMul, err := gorgonia.Mul(sqrDelta, subOneScalar)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't do (y@x)")
+	}
+	reductionDefault := LossReductionMean
+	if len(reduction) != 0 {
+		reductionDefault = reduction[0]
+	}
+	switch reductionDefault {
+	case LossReductionSum:
+		return gorgonia.Sum(matMul)
+	case LossReductionMean:
+		return gorgonia.Mean(matMul)
+	default:
+		return nil, fmt.Errorf("Reduction type %d is not supported", reductionDefault)
+	}
+}
