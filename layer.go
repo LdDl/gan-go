5a9a4ddd33862e3aff7871bc2c25ccef428b2ecf
Dimitrii Lopanov <sexykdi@gmail.com>
2021-10-31 15:03:25 +0300

    provide FWD fn for lstm layer

--- layer.go
+++ layer.go
@@ -43,6 +43,22 @@ type Options struct {
 	EmbeddingSize int
 	// Not used in layers directly. But used for defining parametetrs for certain activation functions (e.g. Softmax)
 	Axis []int
+	// Used in layers: [LSTM]
+	LSTM *LSTMOptions
+}
+
+// LSTMOptions Option for LSTM layer
+type LSTMOptions struct {
+	InputDimension int
+	HiddenSize     int
+	/* Should be Tanh most of times */
+	Activation ActivationFunc
+	/* Should be Sigmoid most of times */
+	ReccurentActivation ActivationFunc
+
+	HiddenNode  *gorgonia.Node
+	DummyHidden *gorgonia.Node
+	DummyCell   *gorgonia.Node
 }
 
 type LayerType uint16
@@ -203,12 +219,132 @@ func (layer *Layer) Fwd(batchSize int, inputs ...*gorgonia.Node) (*gorgonia.Node
 		if err != nil {
 			return nil, errors.Wrap(err, "Can't embedd input of layer")
 		}
+	case LayerLSTM:
+		var input, previousHiddenState, previousCellState *gorgonia.Node
+		if len(inputs) == 1 {
+			input = inputs[0]
+			previousHiddenState = layer.Options.LSTM.DummyHidden
+			previousCellState = layer.Options.LSTM.DummyCell
+		} else if len(inputs) == 3 {
+			input = inputs[0]
+			previousHiddenState = inputs[1]
+			previousCellState = inputs[2]
+		} else {
+			return nil, fmt.Errorf("Layer's type '%d'can handle only 1 or 3 input nodes, got %d", layer.Type, len(inputs))
+		}
+		// Disable activation for layer's output if activation function has't been provided at all
+		if layer.Activation == nil {
+			layer.Activation = NoActivation
+		}
+		// Enable default cell gate activation function if it has't been provided
+		if layer.Options.LSTM.Activation == nil {
+			layer.Options.LSTM.Activation = Tanh
+		}
+		// Enable default input/forget/output gate activation function if it has't been provided
+		if layer.Options.LSTM.ReccurentActivation == nil {
+			layer.Options.LSTM.ReccurentActivation = Sigmoid
+		}
+		sequences := input.Shape()[0]
+		outputs := make([]*gorgonia.Node, sequences)
+		hiddenSize := layer.Options.LSTM.HiddenSize
+		if layer.BiasNode == nil {
+			// Extra help if bias node has't been provided
+			biasShape := tensor.Shape{1, 1, layer.Options.LSTM.HiddenSize * 4}
+			layer.BiasNode = gorgonia.NewTensor(input.Graph(), gorgonia.Float64, 3, gorgonia.WithShape(biasShape...), gorgonia.WithInit(gorgonia.Zeroes()))
+		}
+		for i := range outputs {
+			sequence, err := gorgonia.Slice(input, gorgonia.S(i), nil, nil)
+			if err != nil {
+				return nil, errors.Wrapf(err, "Can't slice layer's input of shape %v", input.Shape())
+			}
+			sequenceReshaped, err := gorgonia.Reshape(sequence, tensor.Shape{1, sequence.Shape()[0], sequence.Shape()[1]})
+			if err != nil {
+				return nil, errors.Wrapf(err, "Can't reshape sliced shape %v as %v", sequence.Shape(), tensor.Shape{1, sequence.Shape()[0], sequence.Shape()[1]})
+			}
+			sequenceApplyWeights, err := gorgonia.BatchedMatMul(sequenceReshaped, layer.WeightNode)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call BatchedMatMul() for [Sequence ⋅ Weights]")
+			}
+			previousHiddenState, err = gorgonia.BatchedMatMul(previousHiddenState, layer.Options.LSTM.HiddenNode)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call BatchedMatMul() for [Previous Hidden State ⋅ LSTM Hidden Node]")
+			}
+			allGates, err := gorgonia.Add(sequenceApplyWeights, previousHiddenState)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call Add() for [(Sequence ⋅ Weights) + Previous Hidden]")
+			}
+			gatesBiased, err := gorgonia.BroadcastAdd(allGates, layer.BiasNode, nil, []byte{0, 1})
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't add biases to gates")
+			}
+			inputGate, err := gorgonia.Slice(gatesBiased, nil, nil, gorgonia.S(0, hiddenSize))
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't slice gates to extract input gate")
+			}
+			inputGateActivated, err := layer.Options.LSTM.ReccurentActivation(inputGate)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call ReccurentActivation() for input gate")
+			}
+			forgetGate, err := gorgonia.Slice(gatesBiased, nil, nil, gorgonia.S(hiddenSize, hiddenSize*2))
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't slice gates to extract forget gate")
+			}
+			forgetGateActivated, err := layer.Options.LSTM.ReccurentActivation(forgetGate)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call ReccurentActivation() for forget gate")
+			}
+			cellGate, err := gorgonia.Slice(gatesBiased, nil, nil, gorgonia.S(hiddenSize*2, hiddenSize*3))
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't slice gates to extract cell gate")
+			}
+			cellGateActivated, err := layer.Options.LSTM.Activation(cellGate)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call Activation() for cell gate")
+			}
+			outputGate, err := gorgonia.Slice(gatesBiased, nil, nil, gorgonia.S(hiddenSize*3, hiddenSize*4))
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't slice gates to extract output gate")
+			}
+			outputGateActivated, err := layer.Options.LSTM.ReccurentActivation(outputGate)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call ReccurentActivation() for output gate")
+			}
+			preserved, err := gorgonia.BroadcastHadamardProd(forgetGateActivated, previousCellState, nil, []byte{0})
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call BroadcastHadamardProd() for [Forget gate ⊙ Previous cell]")
+			}
+			addInfo, err := gorgonia.BroadcastHadamardProd(inputGateActivated, cellGateActivated, nil, []byte{0})
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call BroadcastHadamardProd() for [Input gate ⊙ Cell gate]")
+			}
+			previousCellState, err = gorgonia.Add(preserved, addInfo)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call Add() for [(Forget gate ⊙ Previous cell) + (Input gate ⊙ Cell gate)]")
+			}
+			cellActivated, err := Tanh(previousCellState)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't apply Activation (tanh) to cell")
+			}
+			previousHiddenState, err = gorgonia.BroadcastHadamardProd(outputGateActivated, cellActivated, nil, []byte{0})
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't call BroadcastHadamardProd() for [Output gate ⊙ Activated cell]")
+			}
+			outputs[i] = previousHiddenState
+		}
+		layerNonActivated, err = gorgonia.Concat(0, outputs...)
+		if err != nil {
+			return nil, errors.Wrap(err, "Can't do concatenation on layer's oputputs")
+		}
+		layer.extraNodes = []*gorgonia.Node{
+			previousHiddenState,
+			previousCellState,
+		}
 	default:
 		return nil, fmt.Errorf("Layer's type '%d' (uint16) is not handled", layer.Type)
 	}
 
 	// Bias part
-	if layer.BiasNode != nil {
+	if layer.BiasNode != nil && layer.Type != LayerLSTM {
 		if batchSize < 2 {
 			layerNonActivated, err = gorgonia.Add(layerNonActivated, layer.BiasNode)
 			if err != nil {
